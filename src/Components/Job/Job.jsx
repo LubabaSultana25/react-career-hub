bd787f803bae7c3f6e2c5ef5d6099042459f504a
LubabaSultana25 <tahiaanu.cpi@gmail.com>
2024-03-23 15:54:57 +0600

    feature section adding data

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,14 +1,21 @@
 
 const Job = ({ job }) => {
-    const { logo } = job;
+    const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
         <div className="card card-compact w-96 bg-base-100 shadow-xl">
             <figure><img src={logo} alt="Jobs" /></figure>
             <div className="card-body">
-                <h2 className="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div className="card-actions justify-end">
-                    <button className="btn btn-primary">Buy Now</button>
+                <h2 className="card-title">{job_title}</h2>
+                <p>{company_name} </p>
+
+                <div className="flex gap-3">
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#9873FF] text-[#7E90FE]">
+                        {remote_or_onsite}
+                    </button>
+                    <button  className="px-5 py-2 font-extrabold border rounded border-[#9873FF] text-[#7E90FE]">{job_type}</button>
+                </div>
+                <div className="card-actions">
+                    <button className="btn btn-primary"> View Details  </button>
                 </div>
             </div>
         </div>
