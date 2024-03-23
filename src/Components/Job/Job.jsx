f4463c426372fadd6030e11e475c624d030cb96a
LubabaSultana25 <tahiaanu.cpi@gmail.com>
2024-03-23 18:43:43 +0600

    feature adjust

--- src/Components/Job/Job.jsx
+++ src/Components/Job/Job.jsx
@@ -1,18 +1,25 @@
-
+import {MdOutlineLocationOn } from "react-icons/md";
+import { HiOutlineCurrencyDollar } from "react-icons/hi2";
 const Job = ({ job }) => {
     const { logo, job_title, company_name, remote_or_onsite, location, job_type, salary } = job;
     return (
-        <div className="card card-compact w-96 bg-base-100 shadow-xl">
+        <div className="card card-compact bg-base-100 shadow-xl">
             <figure><img src={logo} alt="Jobs" /></figure>
             <div className="card-body">
                 <h2 className="card-title">{job_title}</h2>
                 <p>{company_name} </p>
 
-                <div className="flex gap-3">
-                    <button className="px-5 py-2 font-extrabold border rounded border-[#9873FF] text-[#7E90FE]">
+                <div >
+                    <button className="px-5 py-2 font-extrabold border rounded border-[#9873FF] text-[#7E90FE] mr-4">
                         {remote_or_onsite}
                     </button>
-                    <button  className="px-5 py-2 font-extrabold border rounded border-[#9873FF] text-[#7E90FE]">{job_type}</button>
+                    <button  className="px-5 py-2 font-extrabold border rounded border-[#9873FF] text-[#7E90FE] mr-4">{job_type}</button>
+                </div>
+
+                <div className="mt-4">
+                    <h2 className="flex"><MdOutlineLocationOn className="text-2xl mr-2"></MdOutlineLocationOn> {location} </h2>
+
+                    <h3><HiOutlineCurrencyDollar className="text-2xl mr-2" /> {salary} </h3>
                 </div>
                 <div className="card-actions">
                     <button className="btn btn-primary"> View Details  </button>
