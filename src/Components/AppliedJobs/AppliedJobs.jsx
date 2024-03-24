ee9de08aed63ddfb7b7251854f33f9ca39104059
LubabaSultana25 <tahiaanu.cpi@gmail.com>
2024-03-24 22:34:44 +0600

    applied jobs

--- src/Components/AppliedJobs/AppliedJobs.jsx
+++ src/Components/AppliedJobs/AppliedJobs.jsx
@@ -1,8 +1,43 @@
+import { useEffect, useState } from "react";
+import { useLoaderData } from "react-router-dom";
+import { getStoredJobApplication } from "../Utility/LocalStorage";
 
 const AppliedJobs = () => {
+    const jobs = useLoaderData();
+
+    const [AppliedJobs, setAppliedJobs] = useState([]);
+    useEffect(() => {
+        const storedJobIds = getStoredJobApplication();
+        if (jobs.length > 0) {
+            const jobsApplied = [];
+            for (const id of storedJobIds) {
+                const job = jobs.find(job => job.id === id);
+                if (job) {
+                    jobsApplied.push(job)
+                }
+            }
+
+            setAppliedJobs(jobsApplied);
+            // const jobsApplied = jobs.filter(job => storedJobIds.includes(job.id));
+            // console.log(jobs,storedJobIds, jobsApplied)
+        }
+    }, [])
+
+
     return (
         <div>
-            <h3>THis is job i do</h3>
+            <h3 className="text-2xl">Job i Applied: {AppliedJobs.length}</h3>
+
+            <ul>
+                {
+                    AppliedJobs.map(job =>
+                        <li key={job.id}>
+                            <span>
+                                {job.job_title}{job.company_name}
+                            </span>
+                        </li>)
+                }
+            </ul>
         </div>
     );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -24,12 +24,13 @@ const router = createBrowserRouter([
       },
       {
         path: "/applied",
-        element: <AppliedJobs></AppliedJobs>
+        element: <AppliedJobs></AppliedJobs>,
+        loader:()=> fetch('/jobs.json') //only load the data you need. do not load all the data
       },
       {
         path: '/job/:id',
         element: <JobDetails></JobDetails>,
-        loader: () =>fetch('../jobs.json')
+        loader: () => fetch('/jobs.json')
       },
     ],
   },
